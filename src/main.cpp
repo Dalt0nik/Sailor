fca68dedb9f4a8622b64f75d0810c9d06047756d
Artiom <h.artiom4ik@gmail.com>
2024-03-27 08:24:30 +0200

    add readSecrets function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
 
+
+std::string readSecrets(const std::string fileName){
+    std::ifstream secretsFile(fileName);
+    std::string api_key;
+    if (secretsFile.is_open()) {
+        std::getline(secretsFile, api_key); 
+        secretsFile.close();
+    } else {
+        std::cout << "Unable to open secrets file";
+    }
+    return api_key;
+}
 int main() {
+    std::string api_key = readSecrets("secrets.txt");
     std::cout << "Hello, World!"<< std::endl;
     return 0;
 }
