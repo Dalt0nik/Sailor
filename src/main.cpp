af1637b6b7ca32fa0460021db323da4889985c57
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2024-03-27 18:29:16 +0200

    finish CMake setup

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,18 @@ std::string readSecrets(const std::string fileName){
         std::getline(secretsFile, api_key); 
         secretsFile.close();
     } else {
-        std::cout << "Unable to open secrets file";
+        std::cout << "Unable to open secrets file" << std::endl;
     }
     return api_key;
 }
-int main() {
-    std::cout << "Test output" << std::endl;
+int main(int argc, char** argv) {
     std::string api_key = readSecrets("secrets.txt");
-    std::cout << "API Key: " << api_key << std::endl;
-    cpr::Response r = cpr::Get(cpr::Url{"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CNY&apikey=" + api_key});
+    if(argc != 3){
+        std::cout << "Usage: " << argv[0] << " <from_currency> <to_currency>" << std::endl;
+        return 1;
+    }
+    std::string url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=" + std::string(argv[1]) + "&to_currency=" + std::string(argv[2]) + "&apikey=" + api_key;
+    cpr::Response r = cpr::Get(cpr::Url{url});
 
     if (r.status_code == 200) { // OK
         std::cout << r.text << std::endl;
@@ -26,7 +29,5 @@ int main() {
         std::cout << "HTTP error occurred: " << r.status_code << std::endl;
     }
 
-    std::cout << "Hello, World!" << std::endl;
-    system("pause");
     return 0;
 }
