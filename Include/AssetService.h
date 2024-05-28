62a1a46af4cdcd6cd8be1f0f2be8c2dd857cbc73
Artiom <h.artiom4ik@gmail.com>
2024-05-28 19:43:20 +0300

    add get expenses, get sells by ticker, getAllTickers functions

--- Include/AssetService.h
+++ Include/AssetService.h
@@ -2,6 +2,7 @@
 
 #include "TradeRepository.h"
 #include <string>
+#include <vector>
 
 class AssetService {
 public:
@@ -12,6 +13,10 @@ public:
     int sell_stock(const std::string& ticker, int amount, double price, const std::string& date);
     double get_total_ticker_value(const std::string& ticker);
     double get_portfolio_value();
+    
+    double get_all_expenses_by_ticker(const std::string &ticker); // we can do these functions private
+    double get_all_sells_by_ticker(const std::string &ticker);    // and create get_zarabotok method
+    std::vector<std::string> GetAllTickers();
 private:
     double get_latest_price(const std::string& symbol);
     TradeRepository& tradeRepository;
