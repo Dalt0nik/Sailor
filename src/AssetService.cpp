96bd924e6154ead89e9696c765eccf526e97029a
Evgenij Shapovalov <zhenya.shapovalov@gmail.com>
2024-05-29 11:05:07 +0300

    fix return values from calculate_ticker_profit

--- src/AssetService.cpp
+++ src/AssetService.cpp
@@ -89,6 +89,8 @@ double AssetService::get_latest_price(const std::string& symbol) {
     std::string url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + this->api_key;
     cpr::Response r = cpr::Get(cpr::Url{ url });
 
+    std::cout << r.text;
+
     if (r.status_code != 200) {
         std::cerr << "HTTP error occurred: " << r.status_code << std::endl;
         return -1.0;
@@ -102,7 +104,7 @@ double AssetService::get_latest_price(const std::string& symbol) {
             latest_price = std::stod(json_response["Global Quote"]["05. price"].get<std::string>());
         }
         else {
-            std::cerr << "Error parsing JSON response or no data available." << std::endl;
+            std::cerr << "Error parsing JSON response or no data available for the latest price." << std::endl;
         }
 
         return latest_price;
@@ -130,10 +132,16 @@ double AssetService::get_price_by_date(const std::string &ticker, const std::str
     std::string url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + ticker + "&outputsize=full&interval=60min&apikey=" + this->api_key + "&month=" + date.substr(0, 7);
     cpr::Response r = cpr::Get(cpr::Url{ url });
 
+    std::cout << "url: " << url << std::endl;
+
+    std::cout << "r: " << r.text << std::endl;
+
     if (r.status_code != 200) {
         std::cerr << "HTTP error occurred: " << r.status_code << std::endl;
         return -1.0;
     }
+
+
 
     try {
         auto json_response = json::parse(r.text);
@@ -145,7 +153,7 @@ double AssetService::get_price_by_date(const std::string &ticker, const std::str
 
             for (auto it = time_series.begin(); it != time_series.end(); ++it) {
                 std::string timestamp = it.key();
-                if (timestamp.substr(0, 10) == date && timestamp.substr(11, 13) == "19") {
+                if (timestamp.substr(0, 10) == date) {
                     if (latest_timestamp.empty() || timestamp > latest_timestamp) {
                         latest_timestamp = timestamp;
                         closing_price = std::stod(it.value()["4. close"].get<std::string>());
@@ -160,7 +168,7 @@ double AssetService::get_price_by_date(const std::string &ticker, const std::str
                 return -1.0;
             }
         } else {
-            std::cerr << "Error parsing JSON response or no data available." << std::endl;
+            std::cerr << "Error parsing JSON response or no data available in the specific date." << std::endl;
             return -1.0;
         }
     } catch (const std::exception& e) {
@@ -176,13 +184,13 @@ double AssetService::calculate_ticker_profit(const std::string &ticker, const st
     
     double total_ticker_value = get_total_ticker_value(ticker);
     if (total_ticker_value == -1.0) {
-        return -1.0; // Indicate error in fetching latest price
+        total_ticker_value = 0;
     }
 
     double sells = get_all_sells_from_date(ticker, date_from);
     double buys = get_all_buys_from_date(ticker, date_from);
 
-    if(sells || buys){
+    if(sells == -1|| buys == -1){
         return -1;
     }
 
